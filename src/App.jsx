f47429d75d288b44319ec48f19aca80fb9641d09
Dastagir2k <dastagir2k@gmail.com>
2024-02-29 21:10:53 +0530

    finised Skills section

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,15 @@ import "./App.css"
 import About from "./components/About"
 import Header from "./components/Header"
 import Hero from "./components/Hero"
+import Skills from "./components/Skills"
 
 const App = () => {
   return (
     <div>
-      
       <Header/>
       <Hero/>
       <About/>
+      <Skills/>
     </div>
   )
 }
--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,20 +1,26 @@
-import AboutImg from "../assets/about.png";
-const About = () => {
-  return (
-    <section className="flex flex-col md:flex-row justify-center bg-primary h-screen">
-      <div className="md:w-1/2 p-10 flex justify-center h-[580px] mt-10">
-        <img src={AboutImg} />
-      </div>
-      <div className="md:w-1/2 flex justify-center text-white p-5">
-        <div className="flex flex-col justify-center">
-          <h1 className="text-4xl  border-b-4 border-[#3d3636] w-[170px] mb-5 font-bold">About me</h1>
-          <p className=" text-2xl mb-5">Hi, My name is Dastagir Ahmed. Im a Full-stack developer. I Built beautiful Webstites with React.js and Tailwind css  </p>
-          <p className="text-2xl mb-5">I am proficient in Frontend skills like React.js, Redux, Redux Tool Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.</p>
-          <p className="text-2xl">In backend I know Node.js, Express.js, MongoDB,  Mongoose , Mysql , Docker</p>
-        </div>
-      </div>
-    </section>
-  );
-};
 
-export default About;
+
+
+import AboutImg from '../assets/about.png';
+
+export default function About () {
+    const config  = {
+        line1: 'Hi, My name is JV Logesh. I am a Full stack web developer. I built beautiful Websites with React.js , Tailwind CSS + Bootstrap',
+        line2: 'I am proficient in Frontend skills like React.js, Redux, Redux Tool Kit, Axios, Tailwind CSS, SaSS, Css3 and many more.',
+        line3: 'In backend I know Node.js, Express.js, MongoDB, and Mongoose'
+    }
+
+    return <section className='flex flex-col md:flex-row bg-gradient-to-b h-screen from-gray-800 to-black px-5' id='about'>
+        <div className='py-5 md:w-1/2'>
+            <img src={AboutImg} />
+        </div>
+        <div className='md:w-1/2 flex justify-center'>
+            <div className='flex flex-col justify-center text-white'>
+                <h1 className='text-4xl border-b-4 border-primary mb-5 w-[170px] font-bold'>About Me</h1>
+                <p className='pb-5'>{config.line1}</p>
+                <p className='pb-5'>{config.line2}</p>
+                <p className='pb-5'>{config.line3}</p>
+            </div>
+        </div>
+    </section>
+}
