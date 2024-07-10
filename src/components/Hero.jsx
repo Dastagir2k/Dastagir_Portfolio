38d1878c9c7d53f6eb7dec6788182e50e03248db
unknown <dastagir2k@gmail.com>
2024-07-10 14:52:59 +0530

    updated project fixed bugs

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -53,12 +53,10 @@ const Hero = () => {
           </a>
         </div>
       </div>
-<<<<<<< HEAD
       <img className="md:w-1/3 relative" src={HeriImg} />
 =======
-      <img className=" md:w-1/3 relative" src={HeriImg} />
->>>>>>> 808ec23c17c1b02f744e73fc4af4455192fbe7ee
     </div>
+
   );
 };
 
